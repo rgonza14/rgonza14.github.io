e16836919a4f0631d880591351cbe0784243cfba
Ruiz Gonzalo <r.gonza1425@gmail.com>
2024-02-08 17:55:26 -0300

    delete: index file for firebase

---
+++ src/firebase/client.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyDF8d09Q5XJ5qXeHpHZ4i99lxBKl1PHRTA',
+    authDomain: 'portfolio-b0697.firebaseapp.com',
+    projectId: 'portfolio-b0697',
+    storageBucket: 'portfolio-b0697.appspot.com',
+    messagingSenderId: '275819567677',
+    appId: '1:275819567677:web:c1686aea7e7a780a9d6854',
+    measurementId: 'G-W963DE4L1D'
+};
+
+export const app = initializeApp(firebaseConfig);
